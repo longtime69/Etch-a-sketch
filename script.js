e17852ee6614bf96898259b61d6f497e2dbeb12b
longtime69 <dcor26@icloud.com>
2025-06-05 03:46:09 -0400

    Resize Button

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 
 const container = document.querySelector(".container");
-
+const button = document.querySelector("button")
 
 for (i = 0; i < 256; i++){
     const div = document.createElement('div');
     div.className = 'fgrid';
-    div.addEventListener('mouseover', () => {
+    div.addEventListener('click', () => {
         div.style.backgroundColor = 'black';
     })
     container.appendChild(div);
 }
+
+button.addEventListener('click', () => {
+    let userSelection = prompt('Enter desired grid size');
+    container.innerHTML = '';
+    for (i = 0; i < userSelection*userSelection; i++){
+        const div = document.createElement('div');
+        div.className = 'fgrid';
+        div.addEventListener('click', () => {
+        div.style.backgroundColor = 'black';
+    })
+    container.appendChild(div);
+    }
+})
 
 
