2fbeb346a761991ee464907b52306dda77bdb824
longtime69 <dcor26@icloud.com>
2025-06-07 03:02:11 -0400

    resize functionality

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 
 const container = document.querySelector(".container");
-const button = document.querySelector("button")
+const button = document.querySelector("button");
+
 
 for (i = 0; i < 256; i++){
     const div = document.createElement('div');
     div.className = 'fgrid';
-    div.addEventListener('click', () => {
-        div.style.backgroundColor = 'black';
+    container.style.gridTemplateColumns = `repeat(${16}, 25px)`;
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'lightblue';
     })
+    
     container.appendChild(div);
 }
 
+
 button.addEventListener('click', () => {
     let userSelection = prompt('Enter desired grid size');
+    if (userSelection > 100){
+        
+    }
     container.innerHTML = '';
+
+
+    container.style.gridTemplateColumns = `repeat(${userSelection}, 25px)`;
+
+
     for (i = 0; i < userSelection*userSelection; i++){
         const div = document.createElement('div');
         div.className = 'fgrid';
-        div.addEventListener('click', () => {
-        div.style.backgroundColor = 'black';
+        div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'lightblue';
     })
     container.appendChild(div);
     }
